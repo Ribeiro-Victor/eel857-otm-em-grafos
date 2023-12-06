74dadf35f13a5aa09cfff4bf33062c58869f4756
Andre Schlaepfer <aaschlaepfer@inoa.com.br>
2023-12-05 23:34:48 -0300

    add: new Solution classes and write to csv

--- helper.cpp
+++ helper.cpp
@@ -4,16 +4,19 @@ using namespace std;
 
 bool DEBUG_ENABLED = false;
 
-void debug_msg(const string& message){
+void debug_msg(const string &message)
+{
     if (DEBUG_ENABLED)
         cout << message << endl;
 }
 
-file_records read_entry_file(string filename){
-    ifstream file(filename); 
-    if (!file.is_open()) {
+file_records read_entry_file(string filename)
+{
+    ifstream file(filename);
+    if (!file.is_open())
+    {
         cerr << "Erro ao abrir o arquivo." << endl;
-        exit (1);
+        exit(1);
     }
 
     // Read first line
@@ -23,16 +26,18 @@ file_records read_entry_file(string filename){
     // Initialize items vector
     vector<item> items(num_items);
     // Define the items' labels, values and weights
-    for (int i = 0; i < num_items; i++) {
+    for (int i = 0; i < num_items; i++)
+    {
         file >> items[i].value;
         items[i].label = i;
     }
-    for (int i = 0; i < num_items; i++) 
+    for (int i = 0; i < num_items; i++)
         file >> items[i].weight;
 
     // Initialize penalties map for each item
-    for (int i = 0; i < num_penalties; i++) {
-        int temp1, penalty, temp2, item_1, item_2; //temp1 e temp2 são descartados
+    for (int i = 0; i < num_penalties; i++)
+    {
+        int temp1, penalty, temp2, item_1, item_2; // temp1 e temp2 são descartados
         file >> temp1 >> penalty >> temp2 >> item_1 >> item_2;
         items[item_1].penalties.push_back(make_pair(item_2, penalty));
         items[item_2].penalties.push_back(make_pair(item_1, penalty));
@@ -43,25 +48,30 @@ file_records read_entry_file(string filename){
     return result;
 }
 
-unordered_map<string, string> read_config_file(const string& filename) {
+unordered_map<string, string> read_config_file(const string &filename)
+{
     unordered_map<string, string> config;
 
     ifstream configFile(filename);
 
-    if (!configFile.is_open()) {
+    if (!configFile.is_open())
+    {
         cerr << "Error opening config file: " << filename << endl;
         return config;
     }
 
     string line;
-    while (getline(configFile, line)) {
-        if (line.empty() || line[0] == '#') {
+    while (getline(configFile, line))
+    {
+        if (line.empty() || line[0] == '#')
+        {
             continue;
         }
 
         istringstream iss(line);
         string key, value;
-        if (getline(iss, key, '=') && getline(iss, value)) {
+        if (getline(iss, key, '=') && getline(iss, value))
+        {
             key.erase(0, key.find_first_not_of(" \t"));
             key.erase(key.find_last_not_of(" \t") + 1);
             value.erase(0, value.find_first_not_of(" \t"));
@@ -75,25 +85,28 @@ unordered_map<string, string> read_config_file(const string& filename) {
     return config;
 }
 
-void log_dataset(file_records* dataset){
-    
-    cout << "Number of itens: " << dataset->num_items << endl; 
+void log_dataset(file_records *dataset)
+{
+
+    cout << "Number of itens: " << dataset->num_items << endl;
     cout << "Knapsack capacity: " << dataset->knapsack_capacity << endl;
 
     cout << "=================VALUE=================" << endl;
-    for (int i=0; i < dataset->num_items; i++)
-        cout << "[" << i << "] = " << dataset->items[i].value << endl; 
+    for (int i = 0; i < dataset->num_items; i++)
+        cout << "[" << i << "] = " << dataset->items[i].value << endl;
     cout << "=================WEIGHT=================" << endl;
-    for (int i=0; i < dataset->num_items; i++)
-        cout << "[" << i << "] = " << dataset->items[i].weight << endl; 
+    for (int i = 0; i < dataset->num_items; i++)
+        cout << "[" << i << "] = " << dataset->items[i].weight << endl;
 
     cout << "=================PENALTIES=================" << endl;
-    for (int i = 0; i < dataset->num_items; i++) {
+    for (int i = 0; i < dataset->num_items; i++)
+    {
         cout << "Penalties for item " << i << " -> [";
         int count = 0;
-        for (auto pair : dataset->items[i].penalties) {
+        for (auto pair : dataset->items[i].penalties)
+        {
             cout << "(" << pair.first << ", " << pair.second << ")";
-            count ++;
+            count++;
             if (count < dataset->items[i].penalties.size())
                 cout << ", ";
         }
@@ -102,9 +115,11 @@ void log_dataset(file_records* dataset){
     cout << "============================================" << endl;
 }
 
-float calculate_penalties(vector<item>* items, vector<int>* solution, int item_index) {
+float calculate_penalties(vector<item> *items, vector<int> *solution, int item_index)
+{
     // If solution is empty
-    if (item_index == 0) {
+    if (item_index == 0)
+    {
         debug_msg("        Solution is empty. Penalty = 0.");
         return 0;
     }
@@ -113,33 +128,35 @@ float calculate_penalties(vector<item>* items, vector<int>* solution, int item_i
     vector<pair<int, int>> penalties = items->at(item_index).penalties;
     float sum = 0;
     debug_msg(to_string(items->at(item_index).label));
-    
+
     // If the item has no pairs with penalties
-    if (penalties.size()==0)
+    if (penalties.size() == 0)
         debug_msg("        Item " + to_string(item_index) + " doesn't have penalty pairs.");
-    
-    for (auto pair: penalties) {
+
+    for (auto pair : penalties)
+    {
         debug_msg(to_string(pair.first));
         if (solution->at(pair.first) == 0)
             debug_msg("        Item " + to_string(pair.first) + " is NOT in the solution.");
-        else {
-            debug_msg("        Item " + to_string(pair.first) + " is in the solution and it's penalty with item "
-            + to_string(item_index) + " is: " + to_string(pair.second));
+        else
+        {
+            debug_msg("        Item " + to_string(pair.first) + " is in the solution and it's penalty with item " + to_string(item_index) + " is: " + to_string(pair.second));
             sum += pair.second;
         }
-        
     }
     return sum;
 }
 
-bool compare(item& a, item& b) {
-    float score_a = static_cast<float>(a.value)/a.weight;
-    float score_b = static_cast<float>(b.value)/b.weight;
+bool compare(item &a, item &b)
+{
+    float score_a = static_cast<float>(a.value) / a.weight;
+    float score_b = static_cast<float>(b.value) / b.weight;
 
     return score_a > score_b;
 }
 
-vector<int> constructive(file_records* dataset){
+vector<int> constructive(file_records *dataset)
+{
     // Iitialize solution vector
     vector<int> solution(dataset->num_items, 0);
     int current_knapsack_weight = 0;
@@ -149,46 +166,54 @@ vector<int> constructive(file_records* dataset){
     debug_msg("Items: " + to_string(dataset->num_items));
     debug_msg("Knapsack capacity: " + dataset->knapsack_capacity);
 
-    // 
+    //
     vector<item> items = dataset->items;
     sort(items.begin(), items.end(), compare);
 
     // Calculate ratio value/weight
-    for (int i=0; i<items.size(); i++) {
+    for (int i = 0; i < items.size(); i++)
+    {
         // If the item fits in the knapsack
         debug_msg("Item: " + to_string(i));
         debug_msg("    Current knapsack weight: " + to_string(current_knapsack_weight));
         debug_msg("    Item weight: " + to_string(items[i].weight));
-        if(current_knapsack_weight + items[i].weight <= dataset->knapsack_capacity) {
+        if (current_knapsack_weight + items[i].weight <= dataset->knapsack_capacity)
+        {
             // Calculate the item score
-            float score = (items[i].value - calculate_penalties(&items, &solution, i))/items[i].weight;
+            float score = (items[i].value - calculate_penalties(&items, &solution, i)) / items[i].weight;
             debug_msg("    Calculate item " + to_string(i) + " score:" + to_string(score));
-            
+
             // Include item in the solution
-            if (score > 0) { //TODO: Incluir mais condições
+            if (score > 0)
+            { // TODO: Incluir mais condições
                 debug_msg("    Include item " + to_string(i) + " in the solution.");
                 solution[items[i].label] = 1;
                 current_knapsack_weight += items[i].weight;
             }
             // Print solution
-            if(DEBUG_ENABLED){
-                cout<<"    Solution = [";
-                for (int i=0; i<solution.size(); i++)
-                    cout<<solution[i];
-                cout<<"]"<<endl;
+            if (DEBUG_ENABLED)
+            {
+                cout << "    Solution = [";
+                for (int i = 0; i < solution.size(); i++)
+                    cout << solution[i];
+                cout << "]" << endl;
             }
         }
     }
     return solution;
 }
 
-int avaliate_solution(vector<int>* solution, file_records* dataset) {
+int avaliate_solution(vector<int> *solution, file_records *dataset)
+{
     debug_msg("********** AVALIATION FUNCTION **********");
     int sum = 0;
-    for (int i=0; i < solution->size(); i++) {
-        if (solution->at(i) == 1) {
+    for (int i = 0; i < solution->size(); i++)
+    {
+        if (solution->at(i) == 1)
+        {
             int total_penalty = 0;
-            for (auto pair: dataset->items[i].penalties){
+            for (auto pair : dataset->items[i].penalties)
+            {
                 if (pair.first < i && solution->at(pair.first) == 1)
                     total_penalty += pair.second;
             }
@@ -198,18 +223,21 @@ int avaliate_solution(vector<int>* solution, file_records* dataset) {
     return sum;
 }
 
-int get_solution_weight(vector<int>* solution, file_records* dataset){
+int get_solution_weight(vector<int> *solution, file_records *dataset)
+{
     int total_weight = 0;
-    for(int i = 0; i < dataset->num_items; i++){
+    for (int i = 0; i < dataset->num_items; i++)
+    {
         total_weight += solution->at(i) * dataset->items[i].weight;
     }
     return total_weight;
-} 
+}
 
-vector<int> generate_neighbor(vector<int>* solution, file_records* dataset){
-    //Generate a viable solution by swapping two itens in backpack
+vector<int> generate_neighbor(vector<int> *solution, file_records *dataset)
+{
+    // Generate a viable solution by swapping two itens in backpack
     debug_msg("********** GENERATE NEIGHBOR FUNCTION **********");
-    srand((unsigned) time(NULL));
+    srand((unsigned)time(NULL));
     int max_iter = 10000;
 
     int n = solution->size();
@@ -217,20 +245,24 @@ vector<int> generate_neighbor(vector<int>* solution, file_records* dataset){
     new_solution = *solution;
 
     int iter = 0;
-    while(iter < max_iter){
+    while (iter < max_iter)
+    {
         int i = rand() % n, j = rand() % n;
-        if(solution->at(i) != solution->at(j)){ //Check if we're swapping itens
+        if (solution->at(i) != solution->at(j))
+        { // Check if we're swapping itens
             new_solution = *solution;
             new_solution[i] = !new_solution[i];
             new_solution[j] = !new_solution[j];
 
-
-            if(get_solution_weight(&new_solution, dataset) <= dataset->knapsack_capacity){ //Check if new solution is viable
+            if (get_solution_weight(&new_solution, dataset) <= dataset->knapsack_capacity)
+            { // Check if new solution is viable
                 int old_score = avaliate_solution(solution, dataset);
                 int new_score = avaliate_solution(&new_solution, dataset);
-                if (old_score < new_score)// Check if new solution is better
+                if (old_score < new_score) // Check if new solution is better
                     return new_solution;
-            }else{
+            }
+            else
+            {
                 new_solution = *solution;
             }
         }
@@ -238,41 +270,47 @@ vector<int> generate_neighbor(vector<int>* solution, file_records* dataset){
     }
 
     return new_solution;
-
 }
 
-vector<int> simulated_annealing(int AS_max, int T_end, int t_0, double alpha, file_records* dataset){
+vector<int> simulated_annealing(int AS_max, int T_end, int t_0, double alpha, file_records *dataset)
+{
     vector<int> curr_solution = constructive(dataset); // curr_solution = s'
-    cout<<"Initial solution: R$"<< avaliate_solution(&curr_solution, dataset) <<endl;
+    cout << "Initial solution: R$" << avaliate_solution(&curr_solution, dataset) << endl;
     vector<int> best_solution = curr_solution; // best_solution = s*
 
     int iter = 0;
     int t = t_0;
     vector<int> new_solution;
-    srand((unsigned) time(NULL));
-    while (t > T_end){
-        while (iter < AS_max){
-            iter ++;
+    srand((unsigned)time(NULL));
+    while (t > T_end)
+    {
+        while (iter < AS_max)
+        {
+            iter++;
             new_solution = generate_neighbor(&curr_solution, dataset); // new_solution = s''
             int old_score = avaliate_solution(&curr_solution, dataset);
             int new_score = avaliate_solution(&new_solution, dataset);
             int delta = new_score - old_score;
-            if (delta > 0){
+            if (delta > 0)
+            {
                 curr_solution = new_solution;
-                int best_score = avaliate_solution(&best_solution, dataset); 
-                if (new_score > best_score){
+                int best_score = avaliate_solution(&best_solution, dataset);
+                if (new_score > best_score)
+                {
                     best_solution = curr_solution;
-                    cout << "Found better solution: R$" << best_score << endl;
+                    // cout << "Found better solution: R$" << new_score << endl;
                 }
-            }else{
+            }
+            else
+            {
                 double rand_between_0_1 = static_cast<double>(rand()) / RAND_MAX;
-                if( rand_between_0_1 < exp( -delta/t ) )
+                if (rand_between_0_1 < exp(-delta / t))
                     curr_solution = new_solution;
             }
         }
         t = alpha * t;
         iter = 0;
-    } 
+    }
 
     return best_solution;
 }
--- main.cpp
+++ main.cpp
@@ -1,35 +1,76 @@
 #include "helper.h"
+#include "Solution.cpp"
 
 using namespace std;
 
-int main() {
+int main()
+{
 
     // Read files
     unordered_map<string, string> config = read_config_file("config.txt");
     file_records dataset = read_entry_file(config["dataset_file"]);
-    
-    log_dataset(&dataset);
-    
-    // Generate first solution
-    // vector<int> solution = constructive(&dataset);
-    
-    // Avaliate solution
-    // int score = avaliate_solution(&solution, &dataset);
 
-    // // Print solution result
-    // cout<<"Initial solution: R$"<<score<<endl;
-    
-    // vector<int> neighbor = generate_neighbor(&solution, &dataset);
-    // for(int i=0; i < 100; i++){
-    //     cout << "Neighbor Value: R$" << avaliate_solution(&neighbor, &dataset) << endl;
-    //     neighbor = generate_neighbor(&neighbor, &dataset);
-    // }
-    int AS_max = stoi(config["AS_max"]);
-    int t_0 = stoi(config["t_0"]);
-    int T_end = stoi(config["T_end"]);
-    double alpha = stod(config["alpha"]);
-    vector<int> final_solution = simulated_annealing(AS_max, T_end, t_0, alpha, &dataset);
-    cout << "Final solution: R$" << avaliate_solution(&final_solution, &dataset) << endl;
-    
+    std::initializer_list<std::string> files_500 = {"sum_instances/500/kpf_1_sum.txt", "sum_instances/500/kpf_2_sum.txt", "sum_instances/500/kpf_3_sum.txt", "sum_instances/500/kpf_4_sum.txt", "sum_instances/500/kpf_5_sum.txt", "sum_instances/500/kpf_6_sum.txt", "sum_instances/500/kpf_7_sum.txt", "sum_instances/500/kpf_8_sum.txt", "sum_instances/500/kpf_9_sum.txt", "sum_instances/500/kpf_10_sum.txt"};
+    std::initializer_list<std::string> files_700 = {"sum_instances/700/kpf_1_sum.txt", "sum_instances/700/kpf_2_sum.txt", "sum_instances/700/kpf_3_sum.txt", "sum_instances/700/kpf_4_sum.txt", "sum_instances/700/kpf_5_sum.txt", "sum_instances/700/kpf_6_sum.txt", "sum_instances/700/kpf_7_sum.txt", "sum_instances/700/kpf_8_sum.txt", "sum_instances/700/kpf_9_sum.txt", "sum_instances/700/kpf_10_sum.txt"};
+    std::initializer_list<std::string> files_800 = {"sum_instances/800/kpf_1_sum.txt", "sum_instances/800/kpf_2_sum.txt", "sum_instances/800/kpf_3_sum.txt", "sum_instances/800/kpf_4_sum.txt", "sum_instances/800/kpf_5_sum.txt", "sum_instances/800/kpf_6_sum.txt", "sum_instances/800/kpf_7_sum.txt", "sum_instances/800/kpf_8_sum.txt", "sum_instances/800/kpf_9_sum.txt", "sum_instances/800/kpf_10_sum.txt"};
+    std::initializer_list<std::string> files_1000 = {"sum_instances/1000/kpf_1_sum.txt", "sum_instances/1000/kpf_2_sum.txt", "sum_instances/1000/kpf_3_sum.txt", "sum_instances/1000/kpf_4_sum.txt", "sum_instances/1000/kpf_5_sum.txt", "sum_instances/1000/kpf_6_sum.txt", "sum_instances/1000/kpf_7_sum.txt", "sum_instances/1000/kpf_8_sum.txt", "sum_instances/1000/kpf_9_sum.txt", "sum_instances/1000/kpf_10_sum.txt"};
+
+    std::map<std::string, std::list<std::string>> dict;
+
+    // // Adicionando dados ao dicionário
+    dict["500"].insert(dict["500"].end(), files_500.begin(), files_500.end());
+    dict["700"].insert(dict["700"].end(), files_700.begin(), files_700.end());
+    dict["800"].insert(dict["800"].end(), files_800.begin(), files_800.end());
+    dict["1000"].insert(dict["1000"].end(), files_1000.begin(), files_1000.end());
+
+    std::list<Solution> solutions;
+
+    for (auto &pair : dict)
+    {
+
+        Solution sol(pair.first);
+
+        std::vector<double> durations;
+        std::vector<int> scores;
+        int counter = 1;
+        for (auto &str : pair.second)
+        {
+            int AS_max = stoi(config["AS_max"]);
+            int t_0 = stoi(config["t_0"]);
+            int T_end = stoi(config["T_end"]);
+            double alpha = stod(config["alpha"]);
+
+            auto start = std::chrono::high_resolution_clock::now();
+            vector<int> final_solution = simulated_annealing(AS_max, T_end, t_0, alpha, &dataset);
+            auto finish = std::chrono::high_resolution_clock::now();
+
+            std::chrono::duration<double> duration = finish - start;
+
+            int final_score = avaliate_solution(&final_solution, &dataset);
+
+            SolutionItem solutionItem(std::to_string(counter), final_score, duration.count());
+
+            sol.addSolutionItem(solutionItem);
+
+            cout << "Final solution: R$" << final_score << endl;
+            counter++;
+        }
+
+        solutions.push_back(sol);
+    }
+
+    for (auto &solution : solutions)
+    {
+        solution.writeItemsToFile();
+    }
+
+    std::ofstream solutionsFile("solutions_summary.csv");
+    solutionsFile << "Qty, Best Solution, Average Solution, Standard Deviation, Average Time, Average Time SD\n";
+    for (auto &solution : solutions)
+    {
+        solution.writePropertiesToFile(solutionsFile);
+    }
+    solutionsFile.close();
+
     return 0;
 }
