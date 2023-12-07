7d4bcebfece4461ac7553be2a1fbe338d0f7945b
Andre Schlaepfer <aaschlaepfer@inoa.com.br>
2023-12-06 23:44:30 -0300

    wip2

--- helper.cpp
+++ helper.cpp
@@ -206,7 +206,7 @@ vector<int> constructive(file_records *dataset)
 
 int avaliate_solution(vector<int> *solution, file_records *dataset)
 {
-    debug_msg("********** AVALIATION FUNCTION **********");
+    debug_msg("********** EVALUATION FUNCTION **********");
     int sum = 0;
     for (int i = 0; i < solution->size(); i++)
     {
@@ -272,109 +272,109 @@ vector<int> generate_neighbor(vector<int> *solution, file_records *dataset)
     return new_solution;
 }
 
-vector<int> generate_neighbor_destroying(vector<int> *solution, file_records *dataset)
-{
-    // Generate a viable solution by removing 5 to 25% itens in backpack
-    debug_msg("********** GENERATE NEIGHBOR FUNCTION **********");
-    srand((unsigned)time(NULL));
-    int max_iter = 10000;
+// vector<int> generate_neighbor_destroying(vector<int> *solution, file_records *dataset)
+// {
+//     // Generate a viable solution by removing 5 to 25% itens in backpack
+//     debug_msg("********** GENERATE NEIGHBOR FUNCTION **********");
+//     srand((unsigned)time(NULL));
+//     int max_iter = 10000;
 
-    int n = solution->size();
-    vector<int> new_solution;
-    new_solution = *solution;
+//     int n = solution->size();
+//     vector<int> new_solution;
+//     new_solution = *solution;
 
-    vector<int> indicesInSolution;
-    vector<int> indicesOutSolution;
-    for (int i = 0; i < solution->size(); ++i)
-    {
-        if (solution->at(i) == 1)
-        {
-            indicesInSolution.push_back(i);
-        }
-        else
-        {
-            indicesOutSolution.push_back(i);
-        }
-    }
+//     vector<int> indicesInSolution;
+//     vector<int> indicesOutSolution;
+//     for (int i = 0; i < solution->size(); ++i)
+//     {
+//         if (solution->at(i) == 1)
+//         {
+//             indicesInSolution.push_back(i);
+//         }
+//         else
+//         {
+//             indicesOutSolution.push_back(i);
+//         }
+//     }
 
-    // Determine the percentage of items to remove (between 5% and 10%)
-    double minPercentage = 0.01;
-    double maxPercentage = 0.01;
-    double range = maxPercentage - minPercentage;
-    double removalPercentage = minPercentage + static_cast<double>(rand()) / RAND_MAX * range;
-    int numToRemove = static_cast<int>(removalPercentage * indicesInSolution.size());
+//     // Determine the percentage of items to remove (between 5% and 10%)
+//     double minPercentage = 0.01;
+//     double maxPercentage = 0.01;
+//     double range = maxPercentage - minPercentage;
+//     double removalPercentage = minPercentage + static_cast<double>(rand()) / RAND_MAX * range;
+//     int numToRemove = static_cast<int>(removalPercentage * indicesInSolution.size());
 
-    // Generate random indices to remove
-    set<int> indicesToRemove;
-    while (indicesToRemove.size() < numToRemove && !indicesInSolution.empty())
-    {
-        int randomIndex = rand() % indicesInSolution.size();
-        auto insertResult = indicesToRemove.insert(indicesInSolution[randomIndex]);
-        // Check if the insertion actually occurred (not a duplicate)
-        if (insertResult.second)
-            indicesInSolution.erase(indicesInSolution.begin() + randomIndex);
-    }
+//     // Generate random indices to remove
+//     set<int> indicesToRemove;
+//     while (indicesToRemove.size() < numToRemove && !indicesInSolution.empty())
+//     {
+//         int randomIndex = rand() % indicesInSolution.size();
+//         auto insertResult = indicesToRemove.insert(indicesInSolution[randomIndex]);
+//         // Check if the insertion actually occurred (not a duplicate)
+//         if (insertResult.second)
+//             indicesInSolution.erase(indicesInSolution.begin() + randomIndex);
+//     }
 
-    // Remove items at the selected indices
-    for (int index : indicesToRemove)
-    {
-        new_solution[index] = 0;
-    }
+//     // Remove items at the selected indices
+//     for (int index : indicesToRemove)
+//     {
+//         new_solution[index] = 0;
+//     }
 
-    // Determine the percentage of items to insert
-    double insertPercentage = minPercentage + static_cast<double>(rand()) / RAND_MAX * range;
-    int numToInsert = static_cast<int>(insertPercentage * indicesOutSolution.size());
+//     // Determine the percentage of items to insert
+//     double insertPercentage = minPercentage + static_cast<double>(rand()) / RAND_MAX * range;
+//     int numToInsert = static_cast<int>(insertPercentage * indicesOutSolution.size());
 
-    // Generate random indices to insert
-    set<int> indicesToInsert;
-    while (indicesToInsert.size() < numToInsert && !indicesOutSolution.empty())
-    {
-        int randomIndex = rand() % indicesOutSolution.size();
-        auto insertResult = indicesToInsert.insert(indicesOutSolution[randomIndex]);
-        // Check if the insertion actually occurred (not a duplicate)
-        if (insertResult.second)
-            indicesOutSolution.erase(indicesOutSolution.begin() + randomIndex);
-    }
+//     // Generate random indices to insert
+//     set<int> indicesToInsert;
+//     while (indicesToInsert.size() < numToInsert && !indicesOutSolution.empty())
+//     {
+//         int randomIndex = rand() % indicesOutSolution.size();
+//         auto insertResult = indicesToInsert.insert(indicesOutSolution[randomIndex]);
+//         // Check if the insertion actually occurred (not a duplicate)
+//         if (insertResult.second)
+//             indicesOutSolution.erase(indicesOutSolution.begin() + randomIndex);
+//     }
 
-    // Insert items at the selected indices
-    for (int index : indicesToInsert)
-    {
-        new_solution[index] = 1;
-    }
+//     // Insert items at the selected indices
+//     for (int index : indicesToInsert)
+//     {
+//         new_solution[index] = 1;
+//     }
 
-    // vector<item> items = dataset->items;
-    // sort(items.begin(), items.end(), compare);
+//     // vector<item> items = dataset->items;
+//     // sort(items.begin(), items.end(), compare);
 
-    // int current_knapsack_weight = get_solution_weight(&new_solution, dataset);
-    // // Calculate ratio value/weight
-    // for (int i=0; i<items.size(); i++) {
-    //     if(indicesToRemove.find(items[i].label) == indicesToRemove.end()){ // If item was not removed from backpack
-    //         if(current_knapsack_weight + items[i].weight <= dataset->knapsack_capacity) {
-    //             // Calculate the item score
-    //             float score = (items[i].value - calculate_penalties(&items, &new_solution, i))/items[i].weight;
+//     // int current_knapsack_weight = get_solution_weight(&new_solution, dataset);
+//     // // Calculate ratio value/weight
+//     // for (int i=0; i<items.size(); i++) {
+//     //     if(indicesToRemove.find(items[i].label) == indicesToRemove.end()){ // If item was not removed from backpack
+//     //         if(current_knapsack_weight + items[i].weight <= dataset->knapsack_capacity) {
+//     //             // Calculate the item score
+//     //             float score = (items[i].value - calculate_penalties(&items, &new_solution, i))/items[i].weight;
 
-    //             // Include item in the solution
-    //             if (score > 0) { //TODO: Incluir mais condições
-    //                 new_solution[items[i].label] = 1;
-    //                 current_knapsack_weight += items[i].weight;
-    //             }
+//     //             // Include item in the solution
+//     //             if (score > 0) { //TODO: Incluir mais condições
+//     //                 new_solution[items[i].label] = 1;
+//     //                 current_knapsack_weight += items[i].weight;
+//     //             }
 
-    //         }
-    //     }
-    // }
+//     //         }
+//     //     }
+//     // }
 
-    // cout << "old solution:" << endl;
-    // for (int i=0; i<solution->size(); i++)
-    //     cout<<solution->at(i);
-    // cout<<"]"<<endl;
+//     // cout << "old solution:" << endl;
+//     // for (int i=0; i<solution->size(); i++)
+//     //     cout<<solution->at(i);
+//     // cout<<"]"<<endl;
 
-    //  cout << "new solution:" << endl;
-    // for (int i=0; i<new_solution.size(); i++)
-    //     cout<<new_solution[i];
-    // cout<<"]"<<endl;
+//     //  cout << "new solution:" << endl;
+//     // for (int i=0; i<new_solution.size(); i++)
+//     //     cout<<new_solution[i];
+//     // cout<<"]"<<endl;
 
-    return new_solution;
-}
+//     return new_solution;
+// }
 
 // vector<int> simulated_annealing(int AS_max, int T_end, int t_0, double alpha, file_records* dataset){
 //     vector<int> curr_solution = constructive(dataset); // curr_solution = s'
@@ -552,6 +552,7 @@ vector<int> simulated_annealing(int AS_max, int T_end, int t_0, double alpha, fi
                 int best_score = avaliate_solution(&best_solution, dataset);
                 if (new_score > best_score)
                 {
+                    // cout << "New Solution: " << new_score << endl;
                     best_solution = curr_solution;
                 }
             }
